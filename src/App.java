961b60d02f1edaa2a37f4334fdc223b1339567af
David Rodriguez <david.rodriguez03@alumnos.ucn.cl>
2023-10-18 15:26:44 -0300

    sis

--- src/App.java
+++ src/App.java
@@ -1,4 +1,10 @@
-
+/**
+ * @author David Jesus Rodriguez Serey
+ *         21.220.555-9
+ *         david.rodriguez03@alumnos.ucn.cl
+ * Carrera: ITI
+ * Profesor: Patricio Rojas Carrasco        
+ */
 
 public class App {
     public static void main(String[] args) {
